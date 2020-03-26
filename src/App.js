08a4ee508564b98b5c9996fe97f1b944bf9eec90
IDStar <bayu.permana@idstar.co.id>
2020-03-26 17:01:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Document } from "react-pdf";
+import pdfjs from 'pdfjs-dist';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+export default () => {
+  const theUrl = window.location.pathname;
+
+  const regex = theUrl.substring(1);
+
+  return (
+    <div>
+      <h3>{`${regex}`}</h3>
+      <Document file={regex} onLoadError={err => alert(err.message)} />
+    </div>
+  );
+}
