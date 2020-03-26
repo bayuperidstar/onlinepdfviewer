fa2213cc39d6b5acd2a6c929c313f2e31cce012e
bayuperidstar <bayu.permana@idstar.co.id>
2020-03-26 17:09:17 +0700

    Updates

---
+++ precache-manifest.a11e57c813652c22a8e92f86fa7cf2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846406d33b0332a4d6304be4222eb556",
+    "url": "/onlinepdfviewer/index.html"
+  },
+  {
+    "revision": "2b8317a7671f0c150ee0",
+    "url": "/onlinepdfviewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d85be3bae2ccde196dc2",
+    "url": "/onlinepdfviewer/static/js/2.908999dc.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/onlinepdfviewer/static/js/2.908999dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8317a7671f0c150ee0",
+    "url": "/onlinepdfviewer/static/js/main.702efdbb.chunk.js"
+  },
+  {
+    "revision": "d8a88ee76b9669e82cc6",
+    "url": "/onlinepdfviewer/static/js/runtime-main.c43ec1e4.js"
+  }
+]);
